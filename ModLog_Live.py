5eb32edd07f8dac4fddc675224621d5b19d6a29e
Jonathan Just <53791986+JonJust@users.noreply.github.com>
2020-03-11 22:25:03 -0400

    Update ModLog_Live.py

--- ModLog_Live.py
+++ ModLog_Live.py
@@ -24,83 +24,78 @@ def main():
     crawlreddit = reddit.subreddit('<INSERT SUBREDDIT TO BE MODERATED HERE>')  # subreddit to be moderated
 
 
-    while True:
+    while True: 
         print('Initiating....')
         try:
             start_time = 0
             for log in reddit.subreddit('fffffffuuuuuuuuuuuu').mod.log(limit=10000):
+                userExists = True
 
-                if True:
-                #if log.created_utc > start_time:
-                    userExists = True
+                if log.action == 'removelink':
+                    try:
+                        str = "https://old.reddit.com/" + log.target_permalink
+                        Submission = reddit.submission(url=str)
+                        print('link url:' + Submission.url)
+                        print('link author:' + Submission.author.name)
+                    except Exception as u:
+                        print('User deleted account!')
+                        print('=====================')
+                        userExists = False
 
-                    if log.action == 'removelink':
-                        try:
-                            str = "https://old.reddit.com/" + log.target_permalink
-                            Submission = reddit.submission(url=str)
-                            print('link url:' + Submission.url)
-                            print('link author:' + Submission.author.name)
+                    if userExists:
+                        try: #If the user is already registered, increment their row. If not, create a new row
+                            cell = sheet.find(Submission.author.name)
+                            colNumber = cell.col
+                            rowNumber = cell.row
+                            value = sheet.cell(rowNumber, (colNumber + 1)).value
+                            if value == '':
+                                value = 0
+                            totalRemoved = int(value)
+                            totalRemoved += 1
+                            sheet.update_cell(rowNumber, (colNumber + 1), totalRemoved)
+                            time.sleep(lagtime)
                         except Exception as u:
-                            print('User deleted account!')
-                            print('=====================')
-                            userExists = False
+                            print('new user!')
+                            value = sheet.acell('Z1').value # Total number of usernames will be stored in Z1
+                            totalUsernames = int(value)
+                            totalUsernames += 1
+                            sheet.update_acell('Z1' , totalUsernames)
+                            sheet.update_cell((totalUsernames + 1), 1, Submission.author.name)
+                            sheet.update_cell((totalUsernames + 1), 2, 1)  # The first infringement is always 1
+                            time.sleep(lagtime)
 
-                        if userExists:
-                            try: #If the user is already registered, increment their row. If not, create a new row
-                                cell = sheet.find(Submission.author.name)
-                                print('Cell Value: ' + cell.value)
-                                colNumber = cell.col
-                                rowNumber = cell.row
-                                value = sheet.cell(rowNumber, (colNumber + 1)).value
-                                if value == '':
-                                    value = 0
-                                totalRemoved = int(value)
-                                totalRemoved += 1
-                                sheet.update_cell(rowNumber, (colNumber + 1), totalRemoved)
-                                time.sleep(lagtime)
-                            except Exception as u:
-                                print('new user!')
-                                value = sheet.acell('Z1').value # Total number of usernames will be stored in Z1
-                                totalUsernames = int(value)
-                                totalUsernames += 1
-                                sheet.update_acell('Z1' , totalUsernames)
-                                sheet.update_cell((totalUsernames + 1), 1, Submission.author.name)
-                                sheet.update_cell((totalUsernames + 1), 2, 1)  # The first infringement is always 1
-                                time.sleep(lagtime)
+                if log.action == 'removecomment':
+                    try:
+                        str = "https://old.reddit.com/" + log.target_permalink
+                        Comment = reddit.comment(url=str)
+                        print('comment body:' + log.target_body)
+                        print('comment author:' + Comment.author.name)
+                    except Exception as u:
+                        print('User deleted account!')
+                        print('=====================')
+                        userExists = False
 
-                    if log.action == 'removecomment':
-                        try:
-                            str = "https://old.reddit.com/" + log.target_permalink
-                            Comment = reddit.comment(url=str)
-                            print('comment body:' + log.target_body)
-                            print('comment author:' + Comment.author.name)
+                    if userExists:
+                        try: #If the user is already registered, increment their row. If not, create a new row
+                            cell = sheet.find(Comment.author.name)
+                            colNumber = cell.col
+                            rowNumber = cell.row
+                            value = sheet.cell(rowNumber, (colNumber + 2)).value
+                            if value == '':
+                                value = 0
+                            totalRemoved = int(value)
+                            totalRemoved += 1
+                            sheet.update_cell(rowNumber, (colNumber + 2), totalRemoved)
+                            time.sleep(lagtime)
                         except Exception as u:
-                            print('User deleted account!')
-                            print('=====================')
-                            userExists = False
-
-                        if userExists:
-                            try: #If the user is already registered, increment their row. If not, create a new row
-                                cell = sheet.find(Comment.author.name)
-                                print('Cell Value: ' + cell.value)
-                                colNumber = cell.col
-                                rowNumber = cell.row
-                                value = sheet.cell(rowNumber, (colNumber + 2)).value
-                                if value == '':
-                                    value = 0
-                                totalRemoved = int(value)
-                                totalRemoved += 1
-                                sheet.update_cell(rowNumber, (colNumber + 2), totalRemoved)
-                                time.sleep(lagtime)
-                            except Exception as u:
-                                print('new user!')
-                                value = sheet.acell('Z1').value # Total number of usernames will be stored in Z1
-                                totalUsernames = int(value)
-                                totalUsernames += 1
-                                sheet.update_acell('Z1' , totalUsernames)
-                                sheet.update_cell((totalUsernames + 1), 1, Comment.author.name)
-                                sheet.update_cell((totalUsernames + 1), 3, 1)  # The first infringement is always
-                                time.sleep(lagtime)
+                            print('new user!')
+                            value = sheet.acell('Z1').value # Total number of usernames will be stored in Z1
+                            totalUsernames = int(value)
+                            totalUsernames += 1
+                            sheet.update_acell('Z1' , totalUsernames)
+                            sheet.update_cell((totalUsernames + 1), 1, Comment.author.name)
+                            sheet.update_cell((totalUsernames + 1), 3, 1)  # The first infringement is always
+                            time.sleep(lagtime)
 
             break
         except Exception as e:
